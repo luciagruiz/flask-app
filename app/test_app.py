42d2728dd98b53c789e70a28b9257d4dd643ed7a
Debian <luciagruiz9@gmail.com>
2025-06-10 11:02:51 +0000

    app modificada

--- app/test_app.py
+++ app/test_app.py
@@ -30,7 +30,7 @@ def test_nombre_env(client, monkeypatch):
     monkeypatch.setenv("NOMBRE", "TestUser")
     response = client.get("/")
     assert response.status_code == 200
-    assert "App de: TestUser" in response.text
+    assert "Aplicación de: TestUser" in response.text
     
 @pytest.fixture
 def client():
